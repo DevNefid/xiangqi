20407df949d6eddc84ed5873136d3a893cd2ecd3
NefidDev <qiutaoyu1@gmail.com>
2026-09-01 15:25:49 +0800

    update

--- server/server.js
+++ server/server.js
@@ -1,16 +1,40 @@
 import express from 'express'
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_ANON_KEY
-)
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-function generate_session_enterid() {
-    return Math.floor(Math.random() * 900000 + 100000)
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error('缺少 Supabase 环境变量！')
+}
+
+async function generate_session_enterid() {
+    while (true) {
+        const enterid = Math.floor(Math.random() * 900000 + 100000)
+        const { data, error } = await supabase
+            .from('sessions')
+            .select('*')
+            .eq('status', 'waiting')
+
+        if (error) {
+            return null
+        }
+
+        let collidedEnterid = false
+        for (const session of data) {
+            if (enterid === session.data.enterid) {
+                collidedEnterid
+            }
+        }
+
+        if (collidedEnterid === false) {
+            return enterid
+        }
+    }
 }
 
 const app = express()
+const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 app.use(express.json())
 
@@ -19,6 +43,11 @@ app.post('/api/create-session', async (req, res) => {
         const host_id = crypto.randomUUID()
         const session_id = crypto.randomUUID()
         const session_enterid = generate_session_enterid()
+
+        if (session_enterid === null) {
+
+        }
+
         const { data, error } = await supabase
             .from('sessions')
             .insert({
@@ -29,7 +58,10 @@ app.post('/api/create-session', async (req, res) => {
                         color: "r"
                     }
                 },
-                status: 'waiting'
+                status: 'waiting',
+                data: {
+                    enterid: session_enterid
+                }
             })
             .select()
 
