477bdd28d98a5f5838790fe0659bd8062de79efa
NefidDev <qiutaoyu1@gmail.com>
2026-09-01 14:27:40 +0800

    update

--- server/server.js
+++ server/server.js
@@ -1,15 +1,29 @@
 import express from 'express'
 
+let sessions = []
+
+function generate_session_id() {
+    return Math.floor(Math.random() * 900000 + 100000)
+}
+
 const app = express()
 
 app.use(express.json())
 
-app.get('/api/test', (req, res) => {
-    res.json({ status: 'ok' })
-})
+app.post('/api/create-session', (req, res) => {
+    try {
+        let session_id = generate_session_id()
+        let host_id = crypto.randomUUID()
+        sessions.push({
+            id: session_id,
+            player1: { id: host_id, color: 'b' },
+            player2: undefined
+        })
+        res.status(200).json({ success: true, host_id: host_id, session_id: session_id })
+    } catch (error) {
+        res.status(500).json({ success: false })
+    }
 
-app.all("/api/*", (req, res) => {
-    res.json({})
 })
 
 export default app
