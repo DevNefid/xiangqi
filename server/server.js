504f1e0c6768f4a83ca1d68ca3e6bcd6318ecabe
NefidDev <qiutaoyu1@gmail.com>
2026-09-01 13:24:27 +0800

    update

--- server/server.js
+++ server/server.js
@@ -8,8 +8,10 @@ app.get('/api/test', (req, res) => {
     res.json({ status: 'ok' })
 })
 
-app.get("/*", (req, res) => {
-    res.json({ a: test })
+app.get("/api/*", (req, res) => {
+    res.status(404).send(`
+        test
+    `)
 })
 
 export default app
